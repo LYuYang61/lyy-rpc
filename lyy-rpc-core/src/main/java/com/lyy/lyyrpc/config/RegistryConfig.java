c37bd2c6f59be7721b43f6d66693f3e6f19a52c6
lian <2242727324@qq.com>
2024-03-26 17:05:49 +0800

    注册中心基本实现

---
+++ lyy-rpc-core/src/main/java/com/lyy/lyyrpc/config/RegistryConfig.java
@@ -0,0 +1,35 @@
+package com.lyy.lyyrpc.config;
+
+/**
+ * @author lian
+ * @title RegistryConfig
+ * @date 2024/3/26 17:04
+ * @description RPC 注册中心配置
+ */
+public class RegistryConfig {
+
+    /**
+     * 注册中心类别
+     */
+    private String registry = "etcd";
+
+    /**
+     * 注册中心地址
+     */
+    private String address = "http://localhost:2380";
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 超时时间（单位毫秒）
+     */
+    private Long timeout = 10000L;
+}
---
+++ lyy-rpc-core/src/main/java/com/lyy/lyyrpc/registry/EtcdRegistry.java
@@ -0,0 +1,12 @@
+package com.lyy.lyyrpc.registry;
+
+/**
+ * @author lian
+ * @title EtcdRegistry
+ * @date 2024/3/26 16:57
+ * @description
+ */
+public class EtcdRegistry {
+
+
+}
